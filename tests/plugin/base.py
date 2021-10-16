ec8724460db2b18b818cef306fb8ed7842530d14
Superskyyy <superskyyy@outlook.com>
2021-10-16 10:30:54 -0400

    Attempt fix
    Signed-off-by: Superskyyy <superskyyy@outlook.com>

--- tests/plugin/base.py
+++ tests/plugin/base.py
@@ -18,7 +18,6 @@
 import inspect
 import os
 import sys
-import time
 from abc import ABC
 from difflib import Differ
 from os.path import dirname
@@ -40,7 +39,7 @@ class TestPluginBase(ABC):
         if expected_file_name is None:
             expected_file_name = os.path.join(dirname(inspect.getfile(self.__class__)), 'expected.data.yml')
 
-        time.sleep(10)
+        # time.sleep(10)  # todo try remove this
 
         with open(expected_file_name) as expected_data_file:
             expected_data = os.linesep.join(expected_data_file.readlines())
