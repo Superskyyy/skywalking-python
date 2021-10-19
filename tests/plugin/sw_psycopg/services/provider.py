eb0d5b2348a7ca67a00c39c5b28d12f82ee1c512
Superskyyy <superskyyy@outlook.com>
2021-10-19 15:43:46 -0400

    fix
    Signed-off-by: Superskyyy <superskyyy@outlook.com>

--- tests/plugin/sw_psycopg/services/provider.py
+++ tests/plugin/sw_psycopg/services/provider.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     @app.route("/users", methods=["POST", "GET"])
     def application():
         time.sleep(0.5)
-        connection = psycopg.connect(host='postgres', user='root', password='root', dbname='admin', port='54321')
+        connection = psycopg.connect(host='postgres', user='root', password='root', dbname='admin', port='5432')
         with connection.cursor() as cursor:
             sql = "select * from user where user = %s"
             cursor.execute(sql, ("root",))
