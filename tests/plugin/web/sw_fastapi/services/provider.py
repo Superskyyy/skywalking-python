b31ed348b58bfa459dfeae37b35102dac92024ce
Superskyyy <superskyyy@outlook.com>
2022-02-11 22:47:47 -0500

    test with localhost not 0.0.0.0
    Signed-off-by: Superskyyy <superskyyy@outlook.com>

--- tests/plugin/web/sw_fastapi/services/provider.py
+++ tests/plugin/web/sw_fastapi/services/provider.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
         level=logging.INFO,
         format='%(asctime)s [%(levelname)s] %(message)s',
         handlers=[
-            logging.FileHandler("debug.log"),
+            logging.FileHandler('debug.log'),
             logging.StreamHandler()
         ]
     )
