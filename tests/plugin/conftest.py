ae3bdd51a630f80744fa4a819f7dac9f6ad5097f
Superskyyy <superskyyy@outlook.com>
2022-02-12 15:21:20 -0500

    fix
    Signed-off-by: Superskyyy <superskyyy@outlook.com>

--- tests/plugin/conftest.py
+++ tests/plugin/conftest.py
@@ -56,8 +56,11 @@ def docker_compose(request, prepare, version):
 
     exception = None
     exception_delay = 100
-    for _ in range(0, 10):
+    for _ in range(0, 1):
         try:
+            time.sleep(100)
+            a,b = compose.get_logs()
+            print(a, b)
             prepare()
             exception = None
             break
