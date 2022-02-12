bc7805a140ddefbb471312e61f0844405c8da12b
Superskyyy <superskyyy@outlook.com>
2022-02-12 00:30:09 -0500

    test with localhost not 0.0.0.0
    Signed-off-by: Superskyyy <superskyyy@outlook.com>

--- tests/plugin/conftest.py
+++ tests/plugin/conftest.py
@@ -56,7 +56,7 @@ def docker_compose(request, prepare, version):
         exception_delay = 100
         for _ in range(1):
             try:
-                #prepare()
+                # prepare()
                 stdout, stderr = compose.get_logs()
                 if stderr:
                     print(f'Errors\n:{stderr}')
@@ -73,7 +73,7 @@ def docker_compose(request, prepare, version):
                 time.sleep(1)
                 exception = e
         if exception:
-            #time.sleep(exception_delay)
+            # time.sleep(exception_delay)
             compose.stop()
             raise Exception(f"""Wait time exceeded {exception_delay} secs. Exception {exception}""")
 
