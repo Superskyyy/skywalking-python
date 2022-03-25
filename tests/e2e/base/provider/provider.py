388b1e1e387e8c0cf6696bff74d72d06b1559282
Superskyyy <superskyyy@outlook.com>
2022-03-25 18:20:41 -0400

    bump up oap effect
    Signed-off-by: Superskyyy <superskyyy@outlook.com>

--- tests/e2e/base/provider/provider.py
+++ tests/e2e/base/provider/provider.py
@@ -43,10 +43,13 @@ class E2EProviderFormatter(logging.Formatter):
     def formatException(self, ei):  # noqa
         """
         Mock user defined formatter which limits the traceback depth
+        to None -> meaning it will not print trackback at all.
+        but the agent should still be able to capture the traceback
+        at default level of 5 by ignoring the cache.
         """
         sio = io.StringIO()
         tb = ei[2]
-        traceback.print_exception(ei[0], ei[1], tb, 1, sio)
+        traceback.print_exception(ei[0], ei[1], tb, None, sio)
         s = sio.getvalue()
         sio.close()
         if s[-1:] == '\n':
